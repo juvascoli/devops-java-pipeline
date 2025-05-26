70bf4ee80680e27d0ab30521ff39d3e287a15412
davignz <werdavid1@gmail.com>
2025-05-25 21:46:58 -0300

    projeto inicial

---
+++ src/main/java/br/com/fiap/moop/MoopApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.moop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoopApplication.class, args);
+	}
+
+}
