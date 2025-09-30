f2eeccfceab83eed0689c0316826927e53a80bc8
juvascoli <jvascoli6@gmail.com>
2025-09-30 09:50:11 -0300

    style: login page

--- src/main/java/br/com/fiap/moop/model/Galpao.java
+++ src/main/java/br/com/fiap/moop/model/Galpao.java
@@ -1,8 +1,17 @@
 package br.com.fiap.moop.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
 
 @Data
 @Entity
@@ -17,9 +26,11 @@ public class Galpao {
     private String nome;
 
     private String localizacao;
-
+    
+    @PositiveOrZero (message="O valor de capacidade não pode ser negativo")
     private int capacidade;
 
+    @PositiveOrZero (message="O valor de motos ocupadas não pode ser negativo")
     @Column(name = "motos_ocupadas")
     private int motosOcupadas;
     
